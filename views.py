07f262c33fb4ccfb34501f3ed44f14c299f8bcc1
Becky Fox <foxy2530@gmail.com>
2022-07-14 16:42:34 +0100

    added meet the teams content, and made some small changes to the css, Final clean of all the files

--- views.py
+++ views.py
@@ -1,35 +1,33 @@
 from flask import Flask, render_template, Blueprint, redirect, url_for
 
-my_view = Blueprint('my_view', __name__)
+my_view = Blueprint("my_view", __name__)
 
-@my_view.route('/')
+@my_view.route("/")
 def index():
     return render_template("index.html")
 
-@my_view.route('/teams')
+@my_view.route("/teams")
 def teams():
     return render_template("teams.html")
 
-@my_view.route('/cars')
+@my_view.route("/cars")
 def cars():
     return render_template("cars.html")
 
-@my_view.route('/calender')
+@my_view.route("/calender")
 def calender():
     return render_template("calender.html")
 
-@my_view.route('/circuits')
+@my_view.route("/circuits")
 def circuits():
     return render_template("circuits.html")
 
-@my_view.route('/driver')
+@my_view.route("/driver")
 def driver():
     return render_template("driver.html")
 
-
-@my_view.route('/home')
-@my_view.route('/js')
-@my_view.route('/javascript')
-
+@my_view.route("/home")
+@my_view.route("/js")
+@my_view.route("/javascript")
 def about_redirect():
     return redirect(url_for("my_view.index"))
